4f3ec01303d2180b210975276dfcf68d3ea41a75
Hari kumar <hktangudu@gmail.com>
2015-09-10 12:50:18 +0530

    app finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,19 @@ Rails.application.routes.draw do
 
   #get 'users/new'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  #resources :users
 
   resources :sessions, only:[:new, :create, :destroy]
 
   resources :microposts, only: [:create, :destroy]
+
+  resources :relationships, only: [:create, :destroy]
 
   # get 'static_pages/home'
 
